4247bcc91d04af702f1b2af06298a907a4d11108
tephfkyo1011 <tephfkyo1011@gmail.com>
2026-04-15 14:30:14 +0700

    Update CACHE_VERSION from v14 to v15

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v14'; 
+const CACHE_VERSION = 'v15'; 
 const STATIC_CACHE = `kyogi-static-${CACHE_VERSION}`;
 const DYNAMIC_CACHE = `kyogi-dynamic-${CACHE_VERSION}`;
 const OFFLINE_URL = './offline.html';
