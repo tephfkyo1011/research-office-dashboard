9a2bc3a5a28fdf1ca84666fdc8a610f2adff9e14
tephfkyo1011 <tephfkyo1011@gmail.com>
2026-04-16 05:27:40 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v16'; // 🟢 อัปเดตเป็น v16 เพื่อบังคับเคลียร์แคชเก่า ดึง app.js ตัวใหม่
+const CACHE_VERSION = 'v17'; // 🟢 อัปเดตเป็น v16 เพื่อบังคับเคลียร์แคชเก่า ดึง app.js ตัวใหม่
 const STATIC_CACHE = `kyogi-static-${CACHE_VERSION}`;
 const DYNAMIC_CACHE = `kyogi-dynamic-${CACHE_VERSION}`;
 const OFFLINE_URL = './offline.html';
