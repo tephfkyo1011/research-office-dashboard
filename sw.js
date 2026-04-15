ad7a02618b817dddc75bc26cff8fb8597ef809c7
tephfkyo1011 <tephfkyo1011@gmail.com>
2026-04-16 06:58:17 +0700

    Update CACHE_VERSION and app.js version to v21

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v20'; // 🟢 อัปเดตเป็น v20 เพื่อบังคับเคลียร์แคชเก่า ดึง app.js ตัวใหม่ แก้โค้ดต้องอัปเลขทุกครั้ง
+const CACHE_VERSION = 'v21'; // 🟢 อัปเดตเป็น v20 เพื่อบังคับเคลียร์แคชเก่า ดึง app.js ตัวใหม่ แก้โค้ดต้องอัปเลขทุกครั้ง
 const STATIC_CACHE = `kyogi-static-${CACHE_VERSION}`;
 const DYNAMIC_CACHE = `kyogi-dynamic-${CACHE_VERSION}`;
 const OFFLINE_URL = './offline.html';
@@ -8,12 +8,11 @@ const MAX_DYNAMIC_ITEMS = 100;
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './app.js?v=20', // 🟢 อัปเดตเป็น v20 เพื่อบังคับเคลียร์แคชเก่า ดึง app.js ตัวใหม่ แก้โค้ดต้องอัปเลขทุกครั้ง
+  './app.js?v=21', // 🟢 อัปเดตเป็น v20 เพื่อบังคับเคลียร์แคชเก่า ดึง app.js ตัวใหม่ แก้โค้ดต้องอัปเลขทุกครั้ง
   './offline.html',
   './ping.txt',
   './libs/fuse.min.js',
   './libs/localforage.min.js',
-  './libs/fontawesome/css/all.min.css' // ใช้ Local FontAwesome
 ];
 
 self.addEventListener('message', event => {
