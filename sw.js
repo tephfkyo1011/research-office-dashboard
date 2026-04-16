820b8d30437030ce3f8f354053a82c7fd2176c96
tephfkyo1011 <tephfkyo1011@gmail.com>
2026-04-16 11:05:58 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v25'; // 🟢 อัปเดตเป็น v25 เพื่อบังคับเคลียร์แคชเก่า ดึง app.js ตัวใหม่ แก้โค้ดต้องอัปเลขทุกครั้ง
+const CACHE_VERSION = 'v26'; // 🟢 อัปเดตเป็น v26 เพื่อบังคับเคลียร์แคชเก่า ดึง app.js ตัวใหม่ แก้โค้ดต้องอัปเลขทุกครั้ง
 const STATIC_CACHE = `kyogi-static-${CACHE_VERSION}`;
 const DYNAMIC_CACHE = `kyogi-dynamic-${CACHE_VERSION}`;
 const OFFLINE_URL = './offline.html';
@@ -9,7 +9,7 @@ const STATIC_ASSETS = [
   // --- ⚙️ โซนที่ 1: ไฟล์ระบบหลัก (Core Files) ---
   './',
   './index.html',
-  './app.js?v=25', // ทิ้งไว้แบบนี้ก่อน ทำเสร็จหมดทุกโปรแกรมค่อยมาขยับเลขทีเดียวครับ
+  './app.js?v=26', // 🟢 อัปเดตเป็น v26 เพื่อบังคับเคลียร์แคชเก่า ดึง app.js ตัวใหม่ แก้โค้ดต้องอัปเลขทุกครั้ง
   './offline.html',
   './ping.txt',
 
